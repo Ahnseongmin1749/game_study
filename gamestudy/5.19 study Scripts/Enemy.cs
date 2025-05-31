f29f6db6c0e6feb669e4cf5dc721882bb03ba4b5
= <=>
2025-05-31 12:27:04 +0000

    5월 19일 스터디 스크립트

---
+++ gamestudy/5.19 study Scripts/Enemy.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody enemyRb;
+    private GameObject player;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 lookDirection = (player.transform.position - transform.position).normalized;
+           //enemyRb.AddForce((player.transform.position
+           // - transform.position).normalized * speed);
+        enemyRb.AddForce(lookDirection * speed);
+           //Debug.Log("position °è»ê °ª" + lookDirection);
+        if(transform.position.y < -10)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+    
+
+
+}
